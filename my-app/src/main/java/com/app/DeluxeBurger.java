422682ede9f05ce4b698783bfcb344af6cc5e9f9
truc nguyen <>
2020-05-11 11:53:00 +0200

    added

---
+++ my-app/src/main/java/com/app/DeluxeBurger.java
@@ -0,0 +1,27 @@
+package com.app;
+
+import com.test.Hamburger;
+
+public class DeluxeBurger extends Hamburger {
+
+    public DeluxeBurger (String name, String Bread, String Meat, double Price,
+                         String Chips, String Drinks){
+        super (name, Bread, Meat,Price);
+        setAddition1(Chips,0);
+        setAddition2 (Drinks, 0);
+    }
+
+    @Override
+    public void setAddition3 (String addition3, double addPrice3){
+        System.out.println("Can not be added");
+    }
+
+    @Override
+    public void setAddition4 (String addition4, double addPrice4){
+        System.out.println("Can not be added");
+    }
+    @Override
+    public double getTotalPrice (){
+        return (getPrice());
+    }
+}
---
+++ my-app/src/main/java/com/app/HeathyBurger.java
@@ -0,0 +1,33 @@
+package com.app;
+
+public class HeathyBurger extends Hamburger{
+
+    private String Addition5;
+    private String Addition6;
+    private double addPrice5;
+    private double addPrice6;
+
+    public HeathyBurger(String name, String Meat, double Price){
+        super(name, "Brown rye bread roll",Meat,Price);
+    }
+
+
+
+    public void setAddition5(String addition5, double addPrice5) {
+        Addition5 = addition5;
+        this.addPrice5 = addPrice5;
+    }
+
+    public void setAddition6(String addition6, double addPrice6) {
+        Addition6 = addition6;
+        this.addPrice6 = addPrice6;
+    }
+
+    @Override
+    public double getTotalPrice (){
+       return (getPrice() + getAddPrice1() + getAddPrice2() +getAddPrice3()  +getAddPrice4()+
+               this.addPrice5 + this.addPrice6);
+    }
+
+
+}
